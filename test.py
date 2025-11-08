26949845c2df9ebd4750227940b428af411b126f
Bereket Ketema <bereketketema866@gmail.com>
2025-11-08 10:47:32 -0300

    changed the message

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
-n=input("Enter a list of numbers: ").split(' ')
+n = list(map(int, input("Enter a list of numbers: ").split()))
 l,r=0,1
+count=0
 while r<len(n):
-    if n[l]<=n[r]:
-        l+=1
-        if r==len(n)-1:
-            print("True")
-        r+=1
-    else:
-        print("False")
-        break
-if len(n)==1:
+    if n[l]>n[r]:
+        count+=1
+    l+=1
+    r+=1
+if len(n)==1 or n[0]:
     print("True")
+if count>1:
+    print('False')
